87ab06931a656c5175365a55d0159fbeb2cf2856
zmdavis15 <zmdavis15@gmail.com>
2022-05-29 11:48:10 -0400

    added bootstrap alert success msg

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,9 +13,18 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import environ
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 # environment variables
 env = environ.Env()
