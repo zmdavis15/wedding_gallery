e0c28a76fabad8248bf0d0219f1bce5b03b022cd
zmdavis15 <zmdavis15@gmail.com>
2022-05-30 08:43:39 -0400

    paginatino set to 10

--- layout/views.py
+++ layout/views.py
@@ -7,7 +7,7 @@ class IndexView(ListView):
     template_name = "layout/index.html"
     model = Photo
     ordering = ['-date']
-    paginate_by = 2  # if pagination is desired
+    paginate_by = 10  # if pagination is desired
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
