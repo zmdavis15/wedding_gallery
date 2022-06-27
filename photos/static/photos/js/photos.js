273917e367afb3b7d16e184800498da3f8297084
Zachary <zmdavis15@gmail.com>
2022-06-26 21:56:30 -0400

    added loading message

--- photos/static/photos/js/photos.js
+++ photos/static/photos/js/photos.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 
+  $('#upload_img_form').submit(function(e) {
+    $('#status').show();
+     // e.preventDefault();
+     // $('#modal').modal('hide');
+     // return true;
+ });
 
 });
