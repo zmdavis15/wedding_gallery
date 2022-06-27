262ea6d0f57cfacdd0e00e054440a4ae0f440d06
Zachary <zmdavis15@gmail.com>
2022-06-26 22:07:31 -0400

    modified production static  files location

--- photos/static/photos/js/photos.js
+++ photos/static/photos/js/photos.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
 
   $('#upload_img_form').submit(function(e) {
     $('#status').show();
-     // e.preventDefault();
-     // $('#modal').modal('hide');
-     // return true;
  });
 
 });
