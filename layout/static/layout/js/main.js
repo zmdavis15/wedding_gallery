fad103362ee52f2c6ab39289c6a4bc877abfe1cd
Zachary <zmdavis15@gmail.com>
2022-07-02 10:56:53 -0400

    submit button disabled after submit

---
+++ layout/static/layout/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // hide success message
+  setTimeout(function(){
+    $('.alert').hide();
+  }, 1500);
+
+
+
+});
--- photos/static/photos/js/photos.js
+++ photos/static/photos/js/photos.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 
   $('#upload_img_form').submit(function(e) {
+    e.preventDefault();
+    // show uploading status
     $('#status').show();
+    $(this).closest("form")[0].submit();
+    // disable submit button on press
+    $('#submit_button').attr("disabled", true);
  });
 
 });
